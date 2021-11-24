2fe7122827460250ad990c00f65f90c337dd7e75
Sam Louderback <sam@Kimberlys-Air.lan>
2021-11-24 13:03:50 -0800

    proto

---
+++ des157a/final/proto/script.js
@@ -0,0 +1,11 @@
+(function () {
+  "use strict";
+  console.log("reading js");
+
+  let imageDivs = document.querySelectorAll(".imagesContainer div");
+
+  // Utility function to get a random Int instead of a random float
+  function getRandomInt(maxVal) {
+    return Math.floor(Math.random() * maxVal);
+  }
+})();
